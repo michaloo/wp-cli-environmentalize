e4c5c7e2a16b972f10e4f6288aa8a1c5b2a8fb10
Michał Rączka <me@michaloo.net>
2016-05-08 17:40:51 +0200

    Fix relative require once command

--- command.php
+++ command.php
@@ -4,6 +4,6 @@ if (!defined('WP_CLI')) {
     return;
 }
 
-require_once './src/WP_CLI_Environmentalize.php';
+require_once __DIR__ . '/src/WP_CLI_Environmentalize.php';
 
 WP_CLI::add_command( 'environmentalize', 'WP_CLI_Environmentalize' );
